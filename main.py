d06779ced7a55c236ce26ddfa60918a39c836c13
fenghan <csfenghan@outlook.com>
2021-09-06 10:29:01 +0800

    修改了文件名

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
 import cv2
 import argparse
 import numpy as np
-import sys
-import os
-import math
-from line_fitting import line_fitting
 from timeit import default_timer as timer
 
-class Detector:
-    def __init__(self):
-        pass
+from feature_analysis import findMidLine
+from utils import parseInput, preProcess
+from exposure_analysis import isOverExposure
+
+def detect(img):
+    img_binary = preProcess(img)
+    mid_line_detected, mid_line_fitted = findMidLine(img_binary)
+    confidence = isOverExposure(img, mid_line_detected, mid_line_fitted)
+
+    if confidence < 0.5:
+        print("正常，置信度：{}".format(confidence))
+    else:
+        print("过曝，置信度：{}".format(confidence))
+
+    for point in mid_line_detected:
+        cv2.circle(img, point, 0, (0, 255, 0))
+    for point in mid_line_fitted:
+        cv2.circle(img, point, 0, (0, 0, 255))
     
-
-
+    return img
 
 def main(opt):
-    detector = Detector()
-    data = detector.parseInput(opt)
-    
+    data = parseInput(opt)
+
     for key in data: 
+        img = cv2.imread(key)
+        output = detect(img)
+        cv2.imshow('output', output)
+        if cv2.waitKey(0) == ord('q'):
+            exit(0)
+
         for value in data[key]:
             img = cv2.imread(value)
-
-            img_binary = detector.preProcess(img)
-            mid_line_detected, mid_line_fitted = detector.findMidLine(img_binary)
-            confidence = detector.isOverExposure(img, mid_line_detected, mid_line_fitted)
-
-            if confidence < 0.5:
-                print("未过曝,置信度:{}".format(confidence))
-            else:
-                print("过曝,置信度:{}".format(confidence))
-
-            for point in mid_line_detected:
-                cv2.circle(img, point, 0, (0, 255, 0))
-            for point in mid_line_fitted:
-                cv2.circle(img, point, 0, (255, 0, 0))
-
-            cv2.imshow("binary_img", img_binary)
-            cv2.imshow("img", img)
-            if cv2.waitKey(0) == ord("q"):
+            output = detect(img)
+            cv2.imshow('output', output)
+            if cv2.waitKey(0) == ord('q'):
                 exit(0)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
